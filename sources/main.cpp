f7fa2043201933fd5ddc48965d0e9ec3f168caa8
юрий <yury@Air-urij.Dlink>
2021-01-23 05:02:46 +1200

    version 2

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,3 +1,3 @@
-// Copyright 2020 Your Name <your_email>
+// Copyright 2020 Shatskov12 <Qweser2016@mail.ru>
 
-#include <header.hpp>
+#include <smart_pointer.hpp>
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,61 @@
-// Copyright 2020 Your Name <your_email>
-
+// Copyright 2020 Shatskov12 <Qweser2016@mail.ru>
 #include <gtest/gtest.h>
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+#include "smart_pointer.hpp"
+TEST(Example, EmptyTest) { EXPECT_TRUE(true); }
+
+
+TEST(SharedPtr, constructorWithArgument) {
+int a = 8;
+SharedPtr<int> var(&a);
+ASSERT_EQ(*var, a);
 }
+
+TEST(SharedPtr, constructorWithLValLink) {
+int a = 8;
+SharedPtr var(&a);
+SharedPtr l(var);
+ASSERT_EQ(var.use_count(), 2);
+}
+
+TEST(SharedPtr, constructorWithRValLink) {
+int a = 9;
+SharedPtr var(&a);
+SharedPtr r(std::move(var));
+ASSERT_EQ(var.use_count(), 2);
+}
+
+TEST(SharedPtr, isMoveConstr){
+ASSERT_EQ(std::is_move_constructible<SharedPtr<int>>::value,true);
+}
+
+TEST(SharedPtr, isMoveAssign){
+ASSERT_EQ(std::is_move_assignable<SharedPtr<int>>::value,true);
+}
+
+TEST(SharedPtr, operatorRavnoWithLValLink){
+int a = 32;
+SharedPtr var(&a);
+SharedPtr var1 = var;
+ASSERT_EQ(var.use_count(),2);
+}
+
+TEST(ShredPtr, operatorRavnoWithRValLink){
+int a = 12;
+SharedPtr var(&a);
+SharedPtr var1 = std::move(var);
+ASSERT_EQ(var.use_count(),2);
+}
+
+
+
+
+TEST(SharedPtr, funcSwap){
+int a = 1;
+int b = 2;
+SharedPtr<int> A(&a);
+SharedPtr<int> B(&b);
+B.swap(A);
+ASSERT_EQ(*B, 1);
+}
+
